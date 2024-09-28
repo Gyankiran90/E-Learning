27cc174a3979cdd25e9075e2a6e25c12d0ca6639
Gyankiran90 <jeejulagyankirran@gmail.com>
2024-09-28 14:12:57 +0530

    Backend Done !!

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Login from "./Login";
+import Logout from "./Logout";
+import { useAuth } from "../context/AuthProvider";
 
 function Navbar() {
-
+  const [authUser,setAuthUser]=useAuth()
   const [theme, setTheme] = useState(
     localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
   );
@@ -128,6 +130,11 @@ function Navbar() {
       d="M21.64,13a1,1,0,0,0-1.05-.14,10.05,10.05,0,0,1-3.37.73A8.15,10.15,0,0,1,9.08,5.49a8.59,10.59,0,0,1,.25-2A1,1,0,0,0,10,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,10.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,10.11,0,0,1,12.14,19.73Z" />
   </svg>
 </label>
+
+{
+  authUser ? (
+  <Logout/>
+  ) : (
   <div className="">
     <a className="bg-black text-white px-3 py-2 rounded-md hover:bg-slate-800 duration-300 cursor-pointer" 
     onClick={()=>document.getElementById("my_modal_3").showModal()
@@ -136,6 +143,7 @@ function Navbar() {
       </a>
       <Login />
   </div>
+   )}
     </div>
     </div>
     </div>
